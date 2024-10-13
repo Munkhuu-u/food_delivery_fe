9dc92a398d12207645b6b0db202fd761b44fed30
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2024-10-14 00:41:10 +0800

    layout almost done

--- src/app/(pages)/signup/layout.tsx
+++ src/app/(pages)/signup/layout.tsx
@@ -1,4 +1,5 @@
-import { PineconeIcon } from "./icons";
+import { JSXElementConstructor } from "react";
+import { PineconeIcon, Basket, Person, Search } from "./icons";
 // import {style} from "../../global.css"
 
 export default function DashboardLayout({
@@ -6,17 +7,50 @@ export default function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const navBarLeftButtons: { name: string; icon: JSX.Element | null }[] = [
+    { name: "", icon: <PineconeIcon /> },
+    { name: "НҮҮР", icon: null },
+    { name: "ХООЛНЫ ЦЭС", icon: null },
+    { name: "ХҮРГЭЛТИЙН БҮС", icon: null },
+  ];
+
+  const navBarRightButtons: { name: string; icon: JSX.Element | null }[] = [
+    { name: "Сагс", icon: <Basket /> },
+    { name: "Нэвтрэх", icon: <Person /> },
+  ];
+
   return (
     <section>
       {/* Include shared UI here e.g. a header or sidebar */}
-      <PineconeIcon />
-      <div className="flex flex-row bg-slate-500">
-        <p className="">НҮҮР</p>
-        <p className="">ХООЛНЫ ЦЭС</p>
-        <p className="">ХҮРГЭЛТИЙН БҮС</p>
-        <input placeholder="Хайх"></input>
-        <p>Сагс</p>
-        <p>Нэвтрэх</p>
+      <div className="max-w-[1258px] mx-auto px-[24px] py-[8px] flex flex-row justify-between items-center bg-green-300">
+        <div className="flex flex-row bg-blue-400">
+          <PineconeIcon />
+          {navBarLeftButtons.map((e) => {
+            return (
+              <button className="flex py-[8px] px-[16px] items-center justify-center font-bold text-base leading-[16px]  bg-slate-500 border-[1px] border-purple-500">
+                {e.name}
+              </button>
+            );
+          })}
+        </div>
+        <div className="flex flex-row gap-2">
+          <div className="relative border-2 py-[8px] pl-[48px] pr-[16px] border-darkgray rounded-[8px] bg-white">
+            <div className="absolute left-0">
+              <Search />
+            </div>
+            <input placeholder="Хайх"></input>
+          </div>
+          {navBarRightButtons.map((e) => {
+            return (
+              <div className="flex flex-row bg-red-300 justify-center items-center gap-2 px-[16px] py-[8px]">
+                {e.icon}
+                <button className="flex py-[8px] px-[16px] items-center justify-center font-bold text-base leading-[16px] bg-slate-500">
+                  {e.name}
+                </button>
+              </div>
+            );
+          })}
+        </div>
       </div>
       {children}
     </section>
--- src/app/(pages)/signup/page.tsx
+++ src/app/(pages)/signup/page.tsx
@@ -6,16 +6,17 @@ import React from "react";
 import { useState } from "react";
 import {
   CheckedBoxIcon,
+  CheckBoxIcon,
   ClosedEyeIconPass,
   OpenEyeIconPass,
   ClosedEyeIconPassAgain,
   OpenEyeIconPassAgain,
 } from "./icons";
 // import "../global.css";
-import { Montserrat } from "next/font/google";
-// import { Open_Sans } from "next/font/google";
-// const openSans = Open_Sans({
-const openSans = Montserrat({
+// import { Montserrat } from "next/font/google";
+import { Open_Sans } from "next/font/google";
+const openSans = Open_Sans({
+  // const openSans = Montserrat({
   subsets: ["latin"],
   display: "swap",
 });
@@ -33,7 +34,6 @@ export default function Page({}: Props) {
   const [showPassword, setShowPassword] = useState(false);
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword);
-    console.log("pressed handleClickShowPassword");
   };
 
   const [showPasswordAgain, setShowPasswordAgain] = useState(false);
@@ -41,16 +41,34 @@ export default function Page({}: Props) {
     setShowPasswordAgain(!showPasswordAgain);
   };
 
+  const [checkAgreeTerms, setCheckAgreeTerms] = useState(false);
+  const handleAgreeTerms = () => {
+    setCheckAgreeTerms(!checkAgreeTerms);
+  };
+
+  const [inputPassword, setInputPassword] = useState("");
+  const [displayPassword, setDisplayPassword] = useState("");
+
+  const handlePasswordOnChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const realValue = event.target.value;
+    const maskedValue = "*".repeat(realValue.length);
+
+    setInputPassword(realValue);
+    setDisplayPassword(maskedValue);
+  };
+
   // setShowPassword((showPassword) => !showPassword);
   return (
     <div className="mt-[74px]">
-      <div className="flex flex-col max-w-[440px] mx-auto p-[32px]">
+      <div className="flex flex-col max-w-[440px] mx-auto p-[32px] gap-[48px]">
         <h2
           className={`${openSans.className} flex flex-col items-center font-bold text-3xl`}
         >
           Бүртгүүлэх
         </h2>
-        <div className="flex flex-col space-y-[16px] mt-[48px]">
+        <div className="flex flex-col space-y-[16px]">
           {inputs.map((e) => (
             // eslint-disable-next-line react/jsx-key
             <div className="relative w-full ">
@@ -69,7 +87,7 @@ export default function Page({}: Props) {
                       : "password"
                     : ""
                 }
-              ></input>
+              />
               <div
                 onClick={
                   e.name == "Нууц үг"
@@ -100,11 +118,29 @@ export default function Page({}: Props) {
             </div>
           ))}
         </div>
-        <div>
-          <CheckedBoxIcon width={24} height={24} fill="#1C1B1F" />
-          {/* Property 'checkedBox' does not exist on type 'JSX.IntrinsicElements */}
+        <div className="flex flex-col gap-[32px]">
+          <div
+            className="flex flex-row  py-[11.5px] gap-[8px]"
+            onClick={handleAgreeTerms}
+          >
+            {checkAgreeTerms ? (
+              <CheckedBoxIcon width={24} height={24} fill="#1C1B1F" />
+            ) : (
+              <CheckBoxIcon width={24} height={24} fill="#1C1B1F" />
+            )}
+            {/* Property 'checkedBox' does not exist on type 'JSX.IntrinsicElements */}
+            <p>Үйлчилгээний нөхцөл зөвшөөрөх</p>
+          </div>
+          <button
+            className={
+              checkAgreeTerms
+                ? " bg-enabledGreenFill py-[14.5px] rounded-[4px] text-white w-full"
+                : "bg-disabledGrayFill py-[14.5px] rounded-[4px] text-disabledGrayText w-full"
+            }
+          >
+            Бүртгүүлэх
+          </button>
         </div>
-        <button className="bg-slate-500">Бүртгүүлэх</button>
       </div>
     </div>
   );
